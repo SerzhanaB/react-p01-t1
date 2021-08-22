f7d1292b4cb040f1b37bdbf96cc88a0ebac37768
Serzhana <buraevase@gmail.com>
2021-08-22 11:59:48 +0300

    Сделали задание по 1 практике Подгрузили стили и сделали подсчет суммы

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Card } from 'antd';
+const a=0;
+const b=1;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+      <Card title="Сумма двух элементов" style={{ width: 300 }}>
+      {a}+{b}={a+b}
+      
+    </Card>
     </div>
+
+
   );
 }
 
